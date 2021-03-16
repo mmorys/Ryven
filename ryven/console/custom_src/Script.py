31ed9c1cff426839d36685afdd5ec91e3e8c0d0c
marcin.morys <marcin.m.morys@gmail.com>
2021-03-15 21:03:37 -0400

    Packaged Ryven to where '$ ryven' command on commandline runs the application

---
+++ ryven/console/custom_src/Script.py
@@ -0,0 +1,15 @@
+from .Flow import Flow
+
+# import sys
+# sys.path.append('../Ryven/')
+# sys.path.append('../Ryven/custom_src/script_variables/')
+from .script_variables.VariablesHandler import VariablesHandler
+
+
+class Script:
+    def __init__(self, config, nodes, node_instance_classes):
+        self.name = config['name']
+        self.variables = []
+        self.variables_handler = VariablesHandler(self, config['variables'])
+        self.flow = Flow(self, config['flow'], nodes, node_instance_classes)
+        self.variables_handler.flow = self.flow
